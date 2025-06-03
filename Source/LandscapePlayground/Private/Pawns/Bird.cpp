a01236aa36925c5ecefa97efd181d1346a0fbdc3
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-06-03 23:57:43 +0200

    Add Bird class

---
+++ Source/LandscapePlayground/Private/Pawns/Bird.cpp
@@ -0,0 +1,23 @@
+#include "Pawns/Bird.h"
+
+ABird::ABird()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
