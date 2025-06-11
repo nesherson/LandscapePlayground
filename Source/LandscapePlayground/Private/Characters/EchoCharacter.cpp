13a9bf07f07156894251226c09927ea929513d46
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-06-11 17:30:39 +0200

    Finish enhanced input functionality
    - Add enhanced input to the EchoCharacter
    - Add camera and spring arm to the EchoCharacter
    -

--- Source/LandscapePlayground/Private/Characters/EchoCharacter.cpp
+++ Source/LandscapePlayground/Private/Characters/EchoCharacter.cpp
@@ -1,13 +1,35 @@
 #include "Characters/EchoCharacter.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 AEchoCharacter::AEchoCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength = 350.f;
+	SpringArm->AddLocalRotation(FRotator(0, -20, 0));
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	Camera->SetupAttachment(SpringArm);
 }
 
 void AEchoCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
 }
 
 void AEchoCharacter::Tick(float DeltaTime)
@@ -18,5 +40,35 @@ void AEchoCharacter::Tick(float DeltaTime)
 void AEchoCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveInputAction, ETriggerEvent::Triggered, this, &AEchoCharacter::Move);
+		EnhancedInputComponent->BindAction(LookInputAction, ETriggerEvent::Triggered, this, &AEchoCharacter::Look);
+	}
 }
+
+void AEchoCharacter::Move(const FInputActionValue& InputActionValue)
+{
+	FVector2D MovementVec = InputActionValue.Get<FVector2D>();
+	FVector ForwardVec = GetActorForwardVector();
+	FVector RightVec = GetActorRightVector();
+
+	AddMovementInput(ForwardVec, MovementVec.Y);
+	AddMovementInput(RightVec, MovementVec.X);
+}
+
+void AEchoCharacter::Look(const FInputActionValue& InputActionValue)
+{
+	FVector2D LookVec = InputActionValue.Get<FVector2D>();
+
+	if (GetController())
+	{
+		AddControllerYawInput(LookVec.X);
+		AddControllerPitchInput(LookVec.Y);
+	}
+}
+
+
+
 
--- Source/LandscapePlayground/Public/Characters/EchoCharacter.h
+++ Source/LandscapePlayground/Public/Characters/EchoCharacter.h
@@ -1,8 +1,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Character.h"
 #include "EchoCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class LANDSCAPEPLAYGROUND_API AEchoCharacter : public ACharacter
@@ -13,8 +19,26 @@ public:
 	AEchoCharacter();
 	
 	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
 protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	UInputAction* MoveInputAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	UInputAction* LookInputAction;
+	
 	virtual void BeginPlay() override;
+	void Move(const FInputActionValue& InputActionValue);
+	void Look(const FInputActionValue& InputActionValue);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* Camera;
 };
